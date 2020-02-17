18ae4c2b317b179b191874261de97c9e6f65ecfc
Simon Martinelli <simon@72.services>
2020-02-17 14:32:39 +0100

    Removed array_push

--- employees.php
+++ employees.php
@@ -7,11 +7,11 @@ $result = $conn->query('select id, first_name, last_name from employee') or die(
 
 $employees = [];
 while ($row = $result->fetch_assoc()) {
-    array_push($employees, [
+    $employees[] = [
         'id' => $row['id'],
         'first_name' => $row['first_name'],
         'last_name' => $row['last_name']
-    ]);
+    ];
 }
 
 echo json_encode($employees);
