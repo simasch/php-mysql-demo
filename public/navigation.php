509ea0273c208160f37ef1e9426dc3f34cba9391
Simon Martinelli <simon@72.services>
2020-02-18 16:32:42 +0100

    Moved files

--- public/navigation.php
+++ public/navigation.php
@@ -1,6 +1,6 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="/">
+        <a class="navbar-brand" href="index.php">
             HR
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
@@ -11,7 +11,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item <?= $page == 'index' ? 'active' : '' ?>">
-                    <a class="nav-link" href="/">Home</a>
+                    <a class="nav-link" href="index.php">Home</a>
                 </li>
                 <li class="nav-item <?= $page == 'employees' ? 'active' : '' ?>">
                     <a class="nav-link" href="employees.php">Employees</a>
