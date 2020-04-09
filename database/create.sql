31f25be28ac0ea1ac49e22e05c50de410132798f
Simon Martinelli <simon@72.services>
2020-04-09 14:54:11 +0200

    Added create.sql

---
+++ database/create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE employee (
+   id               INT             NOT NULL AUTO_INCREMENT
+ , first_name       VARCHAR(255)    NOT NULL
+ , last_name        VARCHAR(255)    NOT NULL
+ , date_of_birth    DATE            NOT NULL
+ , PRIMARY KEY (id));
